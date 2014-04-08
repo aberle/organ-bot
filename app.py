5cc254d4be55b621551cc3109c5ea9e12d92beb4
Devon Tivona <devon@tivona.me>
2014-04-07 19:40:49 -0600

    Finish app

--- app.py
+++ app.py
@@ -1,27 +1,37 @@
 from flask import Flask, render_template, url_for
-#from flask_db import Organ
+from flask import request
 app = Flask(__name__, static_url_path='')
 
 class nav:
-    def __init__(self, href, caption):
-        self.href = href
-        self.caption = caption
+		def __init__(self, href, caption):
+				self.href = href
+				self.caption = caption
 
-@app.route('/show/<id>')
-def stl(id=None):
-
-	if (id == '1'):
-		themodel = "eyes.stl"
-	elif(id == '2'):
-		themodel = "legl.stl"
+@app.route('/show')
+def show():
+	organ = request.args.get('organ')
+	
+	if(organ == 'eyes'):
+		name = "eyes"
+		model = "eyes.stl"
+	elif(organ == 'foot'):
+		name = "foot"
+		model = "footm.stl"
+	elif(organ == 'skin'):
+		name = "skin"
+		model = "skins.stl"
 	else:
-		themodel = "Octocat-v1.stl"
-
-	return render_template('viewer.html', model=themodel)
+		name = "octocat"
+		model = "Octocat-v1.stl"
+	return render_template('viewer.html', model=model, name=name)
 
 @app.route('/')
 def search():
-  return render_template('search.html')
+	return render_template('search.html')
+
+@app.route('/buy')
+def buy():
+	return render_template('buy.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+		app.run(debug=True)
